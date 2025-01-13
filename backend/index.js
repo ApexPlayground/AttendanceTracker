65a09255eca7f8db334a608800710543f980b758
Divine <115173705+ApexPlayground@users.noreply.github.com>
2025-01-13 11:51:15 +0000

    path update for deployment

--- backend/index.js
+++ backend/index.js
@@ -84,6 +84,14 @@ app.get('/attendance', async (req, res) => {
     }
 });
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
 
 
 // Default route for testing the server
